d4a5ee752a7a30bb6b80d6fe427ffcd30ce7cf47
Matt Novenstern <fisxoj@gmail.com>
2018-11-07 16:50:30 -0500

    Add a test that BACKTRACE-STRING works

--- test/tests.lisp
+++ test/tests.lisp
@@ -3,15 +3,31 @@
 (deftestsuite generates-backtrace (trivial-backtrace-test)
   ())
 
+(defmacro with-error-output ((condition-variable) &body body)
+  `(handler-case
+       (let ((x 1))
+	 (let ((y (- x (expt 1024 0))))
+	   (declare (optimize (safety 3)))
+	   (/ 2 y)))
+     (error (,condition-variable)
+       (setf output (progn ,@body)))))
+
 (addtest (generates-backtrace)
   test-1
   (let ((output nil))
-    (handler-case 
-	(let ((x 1))
-	  (let ((y (- x (expt 1024 0))))
-	    (declare (optimize (safety 3)))
-	    (/ 2 y)))
-      (error (c)
-	(setf output (print-backtrace c :output nil))))
+
+    (with-error-output (c)
+      (print-backtrace c :output nil))
+
     (ensure (stringp output))
     (ensure (plusp (length output)))))
+
+
+(addtest (backtrace-string)
+  test-2
+  (let ((output nil))
+
+    (with-error-output (c)
+      (backtrace-string c))
+
+    (ensure (stringp output))))
